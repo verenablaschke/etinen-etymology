ae3dafa869cbe14bb890e7f8f932aa6377033f86
Verena Blaschke <verena.blaschke@gmx.de>
2020-03-17 18:45:05 +0100

    Move etinen-etymology's StandaloneFactViewer to etinen repo

--- src/main/java/de/tuebingen/sfs/eie/components/etymology/eval/EtymologyInferenceTest.java
+++ src/main/java/de/tuebingen/sfs/eie/components/etymology/eval/EtymologyInferenceTest.java
@@ -11,7 +11,6 @@ import de.jdellert.iwsa.util.phonsim.PhoneticSimilarityHelper;
 import de.tuebingen.sfs.cldfjava.data.CLDFWordlistDatabase;
 import de.tuebingen.sfs.eie.components.etymology.problems.EtymologyProblem;
 import de.tuebingen.sfs.eie.components.etymology.util.LevelBasedPhylogeny;
-import de.tuebingen.sfs.eie.gui.facts.StandaloneFactViewer;
 import de.tuebingen.sfs.psl.engine.InferenceResult;
 import de.tuebingen.sfs.psl.engine.ProblemManager;
 import de.tuebingen.sfs.psl.engine.RuleAtomGraph;
@@ -22,6 +21,7 @@ import de.tuebingen.sfs.util.SemanticNetwork;
 public class EtymologyInferenceTest {
 
 	public static void main(String[] args) {
+		// For debugging with a GUI, use the EtymologyFactViewer in the etinen repo.
 		String dbDir = "src/test/resources/northeuralex-0.9";
 		String networkEdgesFile = "src/test/resources/etymology/clics2-network-edges.txt";
 		String networkIdsFile = "src/test/resources/etymology/clics2-network-ids.txt";
@@ -56,15 +56,16 @@ public class EtymologyInferenceTest {
 		String[] langs = new String[] { "eng", "deu", "isl", "swe", "nor", "dan", "fra", "spa", "por", "cat", "fra",
 				"ita", "ron", "lat", "lit", "lav", "rus", "bel", "ukr", "pol", "ces", "slv", "slk", "hrv", "nld" };
 		LevelBasedPhylogeny phylogeny = new LevelBasedPhylogeny(4, treeFile, langs);
-		
+
 		InferenceLogger logger = new InferenceLogger();
 		CLDFWordlistDatabase wordListDb = LoadUtils.loadDatabase(dbDir, logger);
 		CorrespondenceModel corres = LoadUtils.loadCorrModel(dbDir, false, tokenizer, logger);
 		PhoneticSimilarityHelper phonSimHelper = new PhoneticSimilarityHelper(new IPATokenizer(), corres);
-		
+
 		ProblemManager problemManager = ProblemManager.defaultProblemManager();
-		SemanticNetwork net = new SemanticNetwork(networkEdgesFile, networkIdsFile, northeuralexConceptsFile, 2);		
-		EtymologyProblem psl = new EtymologyProblem(problemManager.getDbManager(), "EtymologyProblem", wordListDb, phonSimHelper, net, phylogeny);
+		SemanticNetwork net = new SemanticNetwork(networkEdgesFile, networkIdsFile, northeuralexConceptsFile, 2);
+		EtymologyProblem psl = new EtymologyProblem(problemManager.getDbManager(), "EtymologyProblem", wordListDb,
+				phonSimHelper, net, phylogeny);
 		psl.generateDataAtoms(testConcepts);
 		// psl.atoms.fixateAtoms("Wsim", "?", "?");
 		// psl.atoms.fixateAtoms("Semsim", "?", "?");
@@ -72,6 +73,5 @@ public class EtymologyInferenceTest {
 		RuleAtomGraph rag = result.getRag();
 		rag.printToStream(System.out);
 		result.printInferenceValues();
-		StandaloneFactViewer.launchWithData(psl, result);
 	}
 }
