cb4648b7df7dc2cb22a905b741edf6871b363af4
ketxd <kaparina.jekaterina@gmail.com>
2022-05-06 12:02:53 +0200

    Deployment refactorings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,8 @@
 module de.tuebingen.sfs.eie.components.etymology {
-    exports de.tuebingen.sfs.eie.components.etymology.eval;
     exports de.tuebingen.sfs.eie.components.etymology.filter;
     exports de.tuebingen.sfs.eie.components.etymology.ideas;
     exports de.tuebingen.sfs.eie.components.etymology.problems;
-    exports de.tuebingen.sfs.eie.components.etymology.talk.pred;
     exports de.tuebingen.sfs.eie.components.etymology.talk.rule;
-    exports de.tuebingen.sfs.eie.components.etymology.util;
     requires com.fasterxml.jackson.core;
     requires de.tuebingen.sfs.psl;
     requires java.desktop;
